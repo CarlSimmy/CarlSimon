442b0217735821c1458128b3dbacae96e8c2d1e6
aiudex <SiJansson@gmail.com>
2016-06-16 19:19:18 +0200

    Initial Commit

---
+++ app/scripts/main.js
@@ -0,0 +1,79 @@
+'use strict';
+
+function createTrianglePatterns(element, colorX, colorY) {
+
+  let pattern = Trianglify({
+    width: element.offsetWidth,
+    height: element.offsetHeight,
+    cell_size: 110,
+    variance: 1,
+    x_colors: colorX,
+    y_colors: colorY
+  });
+
+  let clsName = '';
+  if ( element.className.includes(' ') ) {
+    clsName = '.' + element.className.split(' ')[1]; // Second classname with a dot before
+  } else {
+    clsName = '.' + element.className;
+  }
+
+  document.querySelector(clsName).appendChild(pattern.svg()); // Appending svgs as childs
+}
+
+
+/* ---------------------- HEADER TRIANGLES START -----------------------------*/
+
+/* Finding all the triangle divs in the header and pusing them into an array */
+function findHeaderTriangles() {
+  let triangleArray = [];
+
+  for ( let i = 1; i <= document.getElementsByClassName('diamond').length; i++ ) {
+    let elementClass = `diamond-${i}`;
+    triangleArray.push(document.getElementsByClassName(elementClass)[0]);
+  }
+
+  return triangleArray;
+}
+
+/* Calling createTrianglePatterns with all the header triangles */
+function createHeaderTriangles() {
+  findHeaderTriangles().forEach((triangle) => {
+    createTrianglePatterns(triangle, 'BuPu', 'Spectral');
+  });
+}
+
+/* Deleting all svgs appended to the header triangle divs (used when re-rendering) */
+function deleteHeaderTriangles() {
+  // Checking if a div actually contains svgs before looping to remove.
+  if ( document.getElementsByClassName('diamond-1')[0].children.length ) {
+    findHeaderTriangles().forEach((triangle) => {
+      triangle.removeChild(triangle.firstChild);
+    });
+  }
+
+  return;
+}
+
+/* Excecutes functions to re-render the triangle header on resize */
+function reRenderHeaderTriangles() {
+  deleteHeaderTriangles();
+  createHeaderTriangles();
+}
+
+/* ----------------------- HEADER TRIANGLES END ------------------------------*/
+
+/* To create the menu triangles */
+function createMenuTriangles() {
+  let elementClass = 'menu-about';
+  createTrianglePatterns(document.getElementsByClassName(elementClass)[0], 'RdYlGn', 'BuPu');
+}
+
+/* To render all triangles, even ones that don't need to scale on resize */
+function initializeTriangles() {
+  reRenderHeaderTriangles();
+  createMenuTriangles();
+}
+
+window.onload = initializeTriangles();
+window.addEventListener('resize', reRenderHeaderTriangles, true);
