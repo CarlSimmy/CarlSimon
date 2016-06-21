661399cef8cb0be0c0fec10f3b452d9ea76cbfbd
Simon Jansson <SimmeJ@Simon.local>
2016-06-21 16:15:13 +0200

    Vertical Rhythm

--- app/scripts/animations.js
+++ app/scripts/animations.js
@@ -1,14 +1,14 @@
 'use strict';
 
 /* GSAP ANIMATIONS */
-TweenLite.to($('.logotype'), 2, {autoAlpha: 1, y: 80, rotation: 0, ease: Power2.easeOut, delay: 0.5}); // Fade logo in on load.
+TweenLite.to($('.logotype'), 2, {autoAlpha: 1, y: 20, rotation: 0, ease: Power2.easeOut, delay: 0.5}); // Fade logo in on load.
 
 /* SCROLLMAGIC ANIMATIONS */
 var controller = new ScrollMagic.Controller();
 
 var testScene = new ScrollMagic.Scene({
   triggerElement: '.menu-about',
-  offset: -50
+  offset: 100
 })
 .setClassToggle('.menu-about', 'fade-out')
 .addIndicators({
