e395612c76bd760b1dfb55d4a11a4a53360f4b1f
Simon Jansson <SimmeJ@Simon.local>
2016-06-23 12:49:13 +0200

    Shining

--- app/scripts/animations.js
+++ app/scripts/animations.js
@@ -1,11 +1,21 @@
 'use strict';
 
+/* INTERVALS TO TOGGLE SHINE CLASS ON CRYSTALS */
+var shineDivTypes = ['normal', 'reverse'];
+var shineTypes = ['shine'];
+
+setInterval(() => {
+  let currentShineDiv = $('.shineDiv')[Math.floor((Math.random() * $('.shineDiv').length) + 0)];
+  currentShineDiv.className = 'shineDiv ' + shineDivTypes[Math.floor((Math.random() * shineDivTypes.length) + 0)];
+  currentShineDiv.classList.toggle('shine');
+}, Math.floor((Math.random() * 6200) + 3400));
+
 // setInterval(() => {
-//   $('.lol').addClass
-// })
+//   $('.shineDiv')[Math.floor((Math.random() * $('.shineDiv').length) + 0)].classList.toggle('shine');
+// }, Math.floor((Math.random() * 10000) + 7100));
 
 /* GSAP ANIMATIONS */
-TweenLite.to(zest('.logotype'), 2, {autoAlpha: 1, y: 20, rotation: 0, ease: Power2.easeOut, delay: 0.5}); // Fade logo in on load.
+TweenLite.to($('.logotype'), 2, { autoAlpha: 1, y: -25, rotation: 0, ease: Power2.easeOut, delay: 0.5 }); // Fade logo in on load.
 
 /* SCROLLMAGIC ANIMATIONS */
 var controller = new ScrollMagic.Controller();
@@ -13,11 +23,8 @@ var controller = new ScrollMagic.Controller();
 var testScene = new ScrollMagic.Scene({
   triggerElement: '.menu-about',
   offset: 100
-})
-.setClassToggle('.menu-about', 'fade-out')
-.addIndicators({
+}).setClassToggle('.menu-about', 'fade-out').addIndicators({
   name: 'Fade out',
   colorTrigger: 'black',
-  indent: 10,
-})
-.addTo(controller);
+  indent: 10
+}).addTo(controller);
